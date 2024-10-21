a929abe24b7ee979235954c84db9b8a1015033f1
Ryan Hinds <fossryanh@gmail.com>
2024-10-21 16:00:48 -0600

    started work on health system

--- Pumpkin Boy/Assets/Scripts/Objects/BulletTrail.cs
+++ Pumpkin Boy/Assets/Scripts/Objects/BulletTrail.cs
@@ -9,7 +9,11 @@ public class BulletTrail : MonoBehaviour
     [SerializeField] private GameObject hitEffect;
     [SerializeField] private float _speed = 50f;
 
-    public static event Action TriggerHit;
+    public static event HitEvent TriggerHit;
+
+    public delegate void HitEvent(HitType hit, int amount);
+
+    int _damageAmount;
     
     bool _isDestroyed = false;
 
@@ -25,17 +29,43 @@ public class BulletTrail : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+        else
+        {
+            Destroy(this.gameObject, 3f);
+        }
     }
 
-    private void OnCollisionEnter(Collision other)
+    HitType GetHitType(GameObject objHit)
     {
+        if (objHit.CompareTag("Player"))
+        {
+            return HitType.PlayerHit;
+        }
+        else if (objHit.CompareTag("Enemy"))
+        {
+            return HitType.EnemyHit;
+        }
+        else if (objHit.CompareTag("Boss"))
+        {
+            return HitType.BossHit;
+        }
+        else
+        {
+            return HitType.OtherHit;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bullet")) { return; }
+
         if (!_isDestroyed)
         {
             Instantiate(hitEffect, transform.position, Quaternion.identity);
             _isDestroyed = true;
         }
-        TriggerHit?.Invoke();
-
-        Debug.Log($"Hit {other.gameObject.name}");
+        TriggerHit?.Invoke(GetHitType(other.gameObject), _damageAmount);
     }
 }
+
+public enum  HitType { PlayerHit, EnemyHit, BossHit, OtherHit }
